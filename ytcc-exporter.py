df6d3440f1d0e60a383ac192c17122ca77f099e3
madprogramer <themadprogramer@gmail.com>
2020-10-28 13:54:33 +0300

    Can now retrieve last 100 videos of a given channel

--- ytcc-exporter.py
+++ ytcc-exporter.py
@@ -1,12 +1,14 @@
 from datetime import timedelta
 
-from json import dumps
+from json import dumps, loads
 
 from gc import collect
 
 import requests
 
 from time import sleep
+
+import re
 
 # https://docs.python.org/3/library/html.parser.html
 from html.parser import HTMLParser
@@ -57,6 +59,13 @@ def getUserFromUrl(s):
         return splitUp[splitUp.index('user')+1]
     return splitUp[splitUp.index('c')+1]
 
+def getList(s):
+    try:
+        return re.findall(r'list=[^&#]+',s)[0][5:]
+    except:
+        return None
+
+#Unused
 def channelIDInvalid(id):
     if id.find('youtube.com') != -1 or id.find('youtu.be') != -1:
         return True
@@ -66,8 +75,116 @@ def channelIDInvalid(id):
     if r.text.find("empty-channel-banner") != -1:
         return True
     return False
+#Unused END
 
-#Get the channel id of the user
+#Analyze Playlist
+def analyzePlaylist(pID):
+
+    p=[]
+
+    try:
+        #First link is extracted as an html
+        target="https://www.youtube.com/playlist?list={}".format(pID)
+        while True:
+            r=requests.get(target)
+
+            if r.status_code != 200:
+                #t.cancel()
+                print("An error occured while trying to read the playlist...")
+                action=""
+                while action!='r' and action!='a'  :
+                    action = input("Type r to retry or a to abort:\n")
+                    action = action.rstrip().strip()
+                if action == 'r':
+                    #reportProgress()
+                    continue
+                if action == 'a':
+                    return p,1 
+            break
+        plt=r.text
+        imAtSoup = BeautifulSoup(plt,"html.parser")
+
+
+        #Layout HAS CHANGED SINCE 2019, FIXING!
+
+        # Page no longer starts with first few items visible,
+
+        # Hacking into JSON object where first 100 playlist videos are stashed
+        scripts = [ script.contents[0] for script in imAtSoup.find_all('script') if len(script.contents) > 0 and script.contents[0].find("responseContext")!=-1 ]
+        JS = scripts[0]
+
+        #Find the JSONOBJECT PASSED AFTER "window["ytInitialData"] = JSONOBJECT"
+        JSON_START = JS.find("""window["ytInitialData"]""")+26
+        JSON_END = JS.find("""window["ytInitialPlayerResponse"]""")-37
+        JASON=JS[JSON_START:JSON_START+JSON_END]
+
+        #Parse the JSON to collect links
+        #with open("f.json","w") as f:
+        #    f.write(JASON)
+        
+        JSON = loads(JASON)
+
+        BATCH=JSON["contents"]["twoColumnBrowseResultsRenderer"]["tabs"][0]["tabRenderer"]["content"]["sectionListRenderer"]["contents"][0]["itemSectionRenderer"]["contents"][0]["playlistVideoListRenderer"]["contents"]
+        URLs = ["https://www.youtube.com/watch?v={}".format(x["playlistVideoRenderer"]["videoId"]) for x in BATCH]
+        
+        p.extend(URLs)
+        #print(p)
+        #print(SELECTION)
+        #print("LEN: "+str(len(SELECTION)))
+
+        #print("P")
+        #print(p)
+
+        nB = imAtSoup.find_all("button", class_="load-more-button")
+        if len(nB) == 0:
+            return p,0
+        ajaxTarget=nB[0].get("data-uix-load-more-href")
+        target = "https://www.youtube.com{}".format(ajaxTarget)
+        while True:
+            #later playlist links are the internal html of a json
+            #and to be loaded links are stored in a seperate entry of said json 
+            #print(target)
+            #print(plt.find('browse_ajax?ctoken='))
+            Break=False
+            while True:
+                #print(target)
+                r=requests.get(target)
+                #print(r.status_code)
+                if r.status_code != 200:
+                    t.cancel()
+                    print("An error occured while trying to read the upload playlist...")
+                    action=""
+                    while action!='r' and action!='a':
+                        action = input("Type r to retry or a to abort:\n")
+                        action = action.rstrip().strip()
+                    if action == 'r':
+                        reportPlaylistProgress()
+                        continue
+                    if action == 'a':
+                        return p,1
+                break
+            plt=r.json()['content_html']
+            imAtSoup = BeautifulSoup(plt,"html.parser")
+            for link in imAtSoup.find_all("a", class_="pl-video-title-link"):
+                p.append(idExtractor('https://www.youtube.com{}'.format(link.get('href'))))
+            nextLink=r.json()['load_more_widget_html']
+            whereIsSoup = BeautifulSoup(nextLink,"html.parser")
+            #print(whereIsSoup.prettify())
+            nB = whereIsSoup.find_all("button", class_="load-more-button")
+            if len(nB) == 0:
+                return p,0
+            ajaxTarget=nB[0].get("data-uix-load-more-href")
+            target = "https://www.youtube.com{}".format(ajaxTarget)
+            #return 0
+            #print(plt)
+            #return 1
+    except Exception as e:
+        print(e)
+        return [],1
+    else:
+        return p,0
+
+#Get user playlist for "rare" channel URLs
 def getuPLink(customURL,tid):
     userPage=""
     aborted=False
@@ -103,6 +220,236 @@ def getuPLink(customURL,tid):
     #print(uP)
     uPLink = "https://www.youtube.com/playlist?list={}".format("UU"+uP[2:])
     return (uPLink, True)
+
+#DUMP USER PLAYLIST
+def dumpuPL(uPLink):
+    #uPLink = "https://www.youtube.com{}".format(uP.get('href'))
+            
+    lID = getList(uPLink)
+
+    #print(lID)
+    print("getting upload playlist for {}".format(lID))
+
+    #Go to the playlist page using teh id
+    #might not have time to add this
+    #reportPlaylistProgress()
+    p, err = analyzePlaylist(lID)
+    
+    if err==1:
+        return
+    else:
+        print("Public Video Count for this Channel: {}".format(len(p)))
+
+    # print("First Upload Date: {}".format(getVideoDate(p[len(p)-1])))
+    # print("Latest Upload Date: {}".format(getVideoDate(p[0])))
+
+    # print("****************************************************")
+    
+    # print("Optionally you may specify the interval of the videos to be scanned")
+    # print("as two dates in YYYY-MM-DD format on a single line.")
+    # print("Otherwise hit enter")
+    # print("By default the start date will be set to 2005-04-23")
+    # print("And the end date to 2017-05-03 (the day after annotation editing was removed)")
+    # #Query date
+    # #print(getVideoDate(p[0]))
+    # #Format is year-month-day
+    # #4-2-2
+
+    # #index=dateSearch(dateConvert("2017-05-10"),p,0,len(p)-1,False)
+    # sdate=datetime(2005,4,23)
+    # edate=datetime(2017,5,3)
+    # #sdate=datetime(getVideoDate(p[len(p)-1])
+
+    # while True:
+    #     inp = input()
+    #     inp = inp.strip().rstrip()
+        
+    #     if inp=="":
+    #         break
+
+    #     elif not isTwoDates(inp):
+    #         print("Unrecognized input")
+    #         continue
+    #     try:
+    #         sC,eC = getTwoDates(inp)
+    #         #print (sC,eC)
+    #         d1 = dateConvert(sC)
+    #         d2 = dateConvert(eC)
+    #         #print (d1,d2)
+    #         if not validTwoDates(d1,d2):
+    #             print("Invalid interval, make sure you wrote the start date first!")
+    #             continue
+    #         print("Adjusting Interval...")
+    #         sdate = max(d1,sdate)
+    #         edate = min(d2,edate)
+    #         break
+    #     except:
+    #         print("Unable to convert input to date format")
+    #         continue
+
+    # #y,m,d = map( int, sC.split('-') )
+    
+    # #y,m,d = map( int, sC.split('-') )
+    
+    # #indexS = 0
+    # #if sdate != getVideoDate(p[0]):
+    # indexS=dateSearch(edate,p,0,len(p)-1,False)
+
+    # #Special Case:
+    # #If date is 4 23 2005 it's len(p)-1
+    # indexE=len(p)-1
+    # if edate != datetime(2005,4,23):
+    #     indexE=dateSearch(sdate,p,0,len(p)-1,True)
+
+    # #print(indexS,indexE)
+    # #print("error handle index later:")
+
+    # #print(index)
+    # #return
+    # #sdate = 0
+    # #edate = len(p)+1
+
+    # """
+    # while True:
+    #     print("Type the index of the video you want to start from and the index of the last one you'd like to include.")
+    #     print("Or simply press enter without typing anything to scan the full playlist...")
+    #     intervalInput = input()
+    #     if contains2Numbers(intervalInput):
+    #         start,end = get2Numbers(intervalInput)
+    #         if validInterval(start,end,len(p)):
+    #             end+=1
+    #             break
+    #         else:
+    #             print("Please specify a valid interval!")
+    #     elif intervalInput=="":
+    #         start = 0
+    #         end = len(p)+1
+    #         break
+    #     else:
+    #         print("Please type only two numbers representing indices in the playlist if you wish to specify an interval OR press enter to skip.")
+    # #In the case of a playlist sweep ALSO take a snapshot of the playlist
+    
+    # if start == 0 and end == len(p)+1:
+    #     playListSweep = True
+    # """
+    # playListSweep = False
+    # if indexS == 0 and indexE == len(p)-1:
+    #     playListSweep = True
+    # p = p[indexS:indexE+1]
+
+    # #print(indexS,indexE)
+    # #print(p)
+
+    # #Adjust hard limit so it goes beyond the playlist
+    # hardLimitSet = False 
+    # #err = setHardLimit()
+    # #if err ==1:
+    # #continue
+    # setHardLimit()
+
+    # channelLock = False 
+    # err = setChannelLock()
+    # if err ==1:
+    #     continue
+
+    # mode1 = False 
+    # setMode()
+
+
+    # #vID = idExtractor(argument)
+    # #print("vID: {}".format(vID))
+    # #r = annotationsBackedUp(vID)
+    # #if 'closest' in r.json()["archived_snapshots"]:
+    # #   print('That link seems to have been saved.')
+    # #   isY = input('But if you want to make sure that all sublinks have also been saved type y\n')
+    # #   if isY.rstrip().strip() != "y":
+    # #       continue
+
+    # #gather videos linked to from this playlist
+    # #print (p)
+    # #continue
+
+    # Break = False
+    # while True:
+    #     err = activateChannelLock(idExtractor(p[0]))
+    #     if err == 1:
+    #         print("an error occured while trying to access the channel")
+    #         action=""
+    #         while action!='r' and action!='a':
+    #             action = input("Type r to retry or a to abort:\n")
+    #             action = action.rstrip().strip()
+    #         if action == 'r':
+    #             continue
+    #         if action == 'a':
+    #             Break = True
+    #             break
+    #     break
+    # if Break:
+    #     continue
+
+    # err = gatherStartingFromPlaylistVids()
+    # t.cancel()
+    # if err == 1:
+    #     Break=True
+    #     continue
+
+    # #mode 2
+    #     toGather = len(m)
+    #     print("Discovered {} videos...".format(toGather))
+
+    #     i=0
+    #     successes=0
+    #     Break=False
+    #     reportProgress()
+    #     #t.start()
+    #     for ID in m:
+    #         while True:
+    #             code = backUp(ID)
+    #             if code == 1:
+    #                 t.cancel()
+    #                 # #19 is problematic
+    #                 print("https://www.youtube.com/watch?v={} wasn't saved properly".format(ID))
+    #                 action=""
+    #                 while action!='r' and action!='a' and action!='i':
+    #                     action = input("Type r to retry,i to ignore or a to abort:\n")
+    #                     action = action.rstrip().strip()
+    #                 if action == 'r':
+    #                     reportProgress()
+    #                     continue
+    #                 if action == 'i':
+    #                     reportProgress()
+    #                     i+=1
+    #                     break
+    #                 if action == 'a':
+    #                     Break=True
+    #                     playListSweep = False
+    #                     i+=1
+    #                     break
+    #             elif code == 2:
+    #                 print("https://www.youtube.com/watch?v={} is unavailable, skipping...".format(ID))
+    #                 i+=1
+    #                 break
+    #             else:
+    #                 m[ID]=True
+    #                 i+=1
+    #                 successes+=1
+    #                 break
+    #         if Break:
+    #             break
+    #     t.cancel()
+    #     if toGather == 1:
+    #         print("{}/{} is now backed up!".format(successes,toGather))
+    #     print("{}/{} are now backed up!".format(successes,toGather))
+    
+    # #mode 1
+    # else:
+    #     print("DONE!")
+    #     print("{}/{} backed up!".format(successes,len(m)))
+
+
+    # if playListSweep:
+    #     snapShotOfPlaylist(lID)
+    #     print("Took a snapshot of the playlist to let people know its been fully scanned.")
 # Utilities End Here!
 
 class MyHTMLParser(HTMLParser):
@@ -345,7 +692,9 @@ if __name__ == "__main__":
             #print("GOT CHANNEL REQUEST: {}".format(channel))
             tid = getUserFromChannel(channel)
             uPLink = "https://www.youtube.com/playlist?list={}".format("UU"+tid[2:])
+            print("COMMENT THIS OUT LATER A")
             print(uPLink)
+            dumpuPL(uPLink)
 
         #USER OR CUSTOM URL (the trickier case, but crucial for bigger channels)
         elif isUserURL(URL):
@@ -362,7 +711,9 @@ if __name__ == "__main__":
             if not success:
                 print("Failed to retrieve {}".format(tid))
             else:
+                print("COMMENT THIS OUT LATER B")
                 print(uPLink)
+                dumpuPL(uPLink)
 
         #Actual video url
         else: 
